073db3ded3b84ec71947f594c3b917ca532626f1
Andreeez <32762098+Andreeez@users.noreply.github.com>
2018-01-06 12:48:38 +0100

    JSON Fil justerad, script FIL Justerad. Nu funkar det återigen att lägga produkter i varukorgen. Alla produkter är inlagda

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-var userID;
 $(document).ready(function(){
 var categoryList;
 var shoppingCart = [];
-var kundLista;
-userID   = sessionStorage.getItem('userID');
+
 //Fetch huvudkategori
 fetch("huvudkategorier.json")
     .then(function(response){
@@ -42,22 +40,9 @@ fetch("kunder.json")
 
 //Visa huvudkategorier    
     function showMainCategory(){
-        
-
-
-        // $("#header").append("<form><input type='text'>Användarnamn</input></form>");
-        // $("#header").append("<form><input type='password'>Lösenord</input></form>");
-        // $("#header").append("<form><button class='logIn'>Logga in</button></form>");
-        // $(".menyList").append("<li><a href='index.html'>Start</a></li>");
-        var content;
-        content  = '<form><label>Användarnamn</label>';
-        content += '<input id="username" type="text" />';
-        content += '<label>Lösenord</label>';
-        content += '<input id="password" type="password" />';
-        content += '<div class="sigInbutton" ">Logga in</div><form>';
-        
+                
         $('#header').append('<div class="logOut" onclick="signOut()"><i class="fa fa-sign-out"></i><br/>Logga ut</div>');
-        $("#header").append(content);
+      
         
         for(var i = 0; i < categoryList.length; i++){
             var mainCategoryName = "<li class='menuItems' onclick='showUnderCategory(" + [i] + "); showProductList(" + [i] + ")'><a href='#'>" + categoryList[i].mainCategory + "</a></li>";
@@ -68,11 +53,6 @@ fetch("kunder.json")
         $(".menyList").append("<li onclick='showShoppingCart()'><span class='counter'></span><a href='#'>Kundvagn</a></li>");
 
     }
-
-
-
-
-
 
 // Visa underkategorier
     showUnderCategory = function(i){
@@ -130,9 +110,10 @@ fetch("kunder.json")
 
 //Lägg till produkter i localstorage för att visas i Varukorgen
     addToCart = function(i){
-        var product = productList[i]
+        var product = productList[i-1]
 
         shoppingCart.push(product);
+        
 
         $(".counter").html(shoppingCart.length);
 
@@ -145,14 +126,18 @@ fetch("kunder.json")
         $(".content").append("<div class='shoppingCartAllProducts'>"+"</div>")
 
         $(".shoppingCartAllProducts").append("<div class='shoppingCartProduct'> "+ " </div>")
-        
+       
         var json_str = JSON.stringify(shoppingCart);
         localStorage.shoppingCart = json_str;
 
         var loopCart = JSON.parse(localStorage.shoppingCart);
+        console.log("Innan for loop");
+        console.log(loopCart[i]);
         for (var i = 0; i < loopCart.length; i++){
             $(".shoppingCartProduct").append("<div class='shoppingCart'>" + loopCart[i].prodName + " " +  loopCart[i].prodPrice + " kr" + "<a href='#' onClick='delCartItem(" + i + ")'>Ta bort</a>" + "</div>");
-
+            console.log("I for loop");
+            console.log(loopCart[i].prodName);
+            console.log(loopCart[i].id);
         }
         var totalPrice = 0;
         var freightPrice = 55;
@@ -174,98 +159,5 @@ fetch("kunder.json")
         $(".counter").html(shoppingCart.length);
         
         showShoppingCart();
-    }
-
-
-    $(".logOut").hide();
-        
-        if (sessionStorage.username!= null) {//om nån är inne
-            inloggad();
-        } else {
-
-            $(".sigInbutton").click(function(){
-                console.log("hejhej");
-                for(var i = 0; i < kundLista.length; i++){
-                    //var user = listOfKunder[i].email;
-                    //var password = listOfKunder[i].password;
-                
-            
-                if(kundLista[i].email == $("#username").val() && kundLista[i].password == $("#password").val()){
-                    sessionStorage.username = kundLista[i].email;
-                    inloggad();
-                    console.log("rätt");
-                } else {
-                    console.log("fel");
-                    $(".logOut").hide();
-                    alert("Fel lösen! Försök igen");
-                    }
-                }
-            });
-
-        }
-        inloggad(); {
-            $(".logOut").show();
-        }
-
-
-//     signIn = function() {
-
-//         if (sessionStorage.username !=null){
-//             logedIn();
-            
-//         } else {
-//             for(var i = 0; i < kundLista.length; i++){
-//             if(kundLista[i].email == $("#username").val() && kundLista[i].password == $("#password").val() ){
-//                 sessionStorage.username = kundLista[i].email;
-//                 logedIn();
-//             } else {
-//                 showMessage();
-//             }
-        
-//         }
-//     }
-// }
-
-// logedIn = function(){
-//     console.log("hej");
-// }
-
-
-
-    //     var username = $('#username').val().toLowerCase().trim();
-    //     if(kundLista[username]) {
-    //         var password = $('#password').val();
-            
-    //         if(kundLista[username].password == password) {
-    //             sessionStorage.setItem('userID', username);
-    //             window.location.reload();
-                
-    //         } else showMessage('error', 'Fel lösenord.');
-            
-    //     } else showMessage('error', 'Fel användarnamn.');
-    // };
-
-    // var showMessage = function(status, message) {
-    //     $(window).scrollTop(0);
-    //     $('#message').html('<div class="'+status+'">'+message+'</div>');
-    //     $('#message div').slideDown(400);
-    //     $('.'+status).delay(4000).slideUp(400, function(){ $(this).remove(); });
-    // };
-    // signOut = function() {
-    //     sessionStorage.removeItem('userID');
-    //     window.location.reload();
-    // };
-
-    // var logginModal = function() {
-    //     var content;
-    //     content  = '<form><label>Användarnamn</label>';
-    //     content += '<input id="username" type="text" />';
-    //     content += '<label>Lösenord</label>';
-    //     content += '<input id="password" type="password" />';
-    //     content += '<div class="button" onclick="signIn()">Logga in</div><form>';
-    //     $("#header").append(content);
-    // };
-
-
-
+    } 
 });
